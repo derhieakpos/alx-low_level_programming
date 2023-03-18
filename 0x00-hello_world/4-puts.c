3b8451fc5ee356feb01a679eff7fb636037a0aa1
derhieakpos <derhieakpos@gmail.com>
2023-03-18 10:04:03 -0700

    Hello, printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- *main - Entry point
+ * main - Entry point
  *
- * return: Always 0(Success)
+ * Return: Always 0 (Success)
  */
-int main(void);
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
