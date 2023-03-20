786eda6f24dad820844d24a74df06473baad0fcd
derhieakpos <derhieakpos@gmail.com>
2023-03-20 15:18:50 -0700

    Positive, negative, zero fix

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,13 +18,13 @@ int main(void)
 	{
 		printf("%d is zero\n", n);
 	}
-	else if (n < 0)
+	else if (n > 0)
 	{
-		printf("%d is negative\n", n);
+		printf("%d is positive\n", n);
 	}
 	else
 	{
-		printf("%d is positive\n", n);
+		printf("%d is negative\n", n);
 		return (0);
 	}
 }
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	for (n < 10);
+	{
+		printf("n\n");
+	}
+}
