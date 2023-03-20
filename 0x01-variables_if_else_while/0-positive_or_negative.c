e414bf623fad401df992160f3c5e74b81ef60f7a
derhieakpos <derhieakpos@gmail.com>
2023-03-20 08:04:54 -0700

    Compiled Positive and Negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,10 +17,12 @@ int main(void)
 	if (n == 0)
 	{
 		printf("%d is zero\n", n);
-	} else if (n < 0)
+	}
+	else if (n < 0)
 	{
 		printf("%d is negative\n", n);
-	} else
+	}
+	else
 	{
 		printf("%d is positive\n", n);
 		return (0);
