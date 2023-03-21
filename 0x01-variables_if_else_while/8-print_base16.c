1e460eaeebc5aa616a043750543193986ead88b0
derhieakpos <derhieakpos@gmail.com>
2023-03-20 22:00:21 -0700

    Base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+		for (i = 97; i <= 102; i++)
+		{
+			putchar(i);
+		}
+		return (0);
+	}
+}
