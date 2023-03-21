0edf8fd1399c8a2851ce6fd97005564ce84a60e6
derhieakpos <derhieakpos@gmail.com>
2023-03-20 22:23:32 -0700

    Print combo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+		if (i == 57)
+		{
+			break;
+		}
+		putchar(',');
+		putchar(' ');
+	}
+	return (0);
+}
