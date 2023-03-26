cc06d1cb10f92d232977926413417479abdec214
derhieakpos <derhieakpos@gmail.com>
2023-03-26 04:44:15 -0700

    Print alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,9 +9,13 @@ int main(void)
 	int i;
 
 	for (i = 97; i <= 122; i++)
-		for (i = 65; i <= 90; i++)
-		{
-			putchar(i);
-		}
+	{
+		putchar(i);
+	}
+	for (i = 65; i <= 90; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
 	return (0);
 }
