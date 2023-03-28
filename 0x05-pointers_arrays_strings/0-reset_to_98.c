87ca414c806aa863259abbced71c5726f25b8a22
derhieakpos <derhieakpos@gmail.com>
2023-03-28 07:43:35 -0700

    98 battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * pointer - it takes the parameter int with the value of 98
+ *
+ * Return: n
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef main_h
+#define main_h
+
+void reset_to_98(int *n);
+
+#endif
