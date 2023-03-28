826edcd6554134e8a3f72f868f0dda791e78fae6
derhieakpos <derhieakpos@gmail.com>
2023-03-28 16:40:25 -0700

    Reverse strings.

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts - prints a string followed by a new line
+ * @*str: the string
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(str+)+;
+	}
+	_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_rev - imprime en reversa
+ * @s: string
+ * return: 0
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+	int o;
+
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	s--;
+	for (o = i; o > 0; o--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
