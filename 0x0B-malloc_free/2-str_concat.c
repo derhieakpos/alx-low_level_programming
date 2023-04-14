34d1beae44fd63601a1710ce548e27e07c7911f8
derhieakpos <derhieakpos@gmail.com>
2023-04-14 15:24:28 -0700

    He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,12 +1,52 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
- * str_concat - The function that concatenates two strings.
- * @s1: The first string
- * @s2: The second string
- * Return: NULL on failure
+ * str_concat - concatenates s1 to s2.
+ * @s1: first string.
+ * @s2: second string.
  *
+ * Return: pointer to ptr NULL otherwise.
  */
 char *str_concat(char *s1, char *s2)
 {
-	int l;
-	
+	int l, len_tot;
+	char *ptr;
+
+	int len_s1, len_s2, i, j;
+
+	len_s1 = 0;
+	len_s2 = 0;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	for (i = 0; s1[i] != '\0'; i++)
+		len_s1++;
+
+	for (j = 0; s2[j] != '\0'; j++)
+		len_s2++;
+
+	len_tot = len_s1 + len_s2 + 1;
+
+	ptr = (char *)malloc(len_tot * sizeof(char));
+
+	if (ptr)
+	{
+		for (l = 0; l < len_s1; l++)
+		{
+			ptr[l] = s1[l];
+		}
+		for (l = 0; l < len_s2; l++)
+		{
+			ptr[len_s1 + l] = s2[l];
+		}
+		ptr[len_tot - 1] = '\0';
+
+		return (ptr);
+	}
+	return (NULL);
+}
