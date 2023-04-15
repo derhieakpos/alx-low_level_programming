c6570834c88994eea3a4521f96c21f694586deae
derhieakpos <derhieakpos@gmail.com>
2023-04-14 17:50:29 -0700

    Task 0

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,9 +1,5 @@
 #include "main.h"
-/**
- * _isupper - upper case.
- * @c: the string.
- * Return: 1
- */
+
 int _isupper(int c)
 {
 }
--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -1,10 +1,5 @@
 #include "main.h"
-/**
- * _memset - upper case.
- * @dest: the string.
- * @src: the string.
- * Return: 1
- */
+
 char *_memset(char *s, char b, unsigned int n)
 {
 }
