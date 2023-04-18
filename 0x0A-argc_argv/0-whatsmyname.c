85431aa1f3a0ecb086db0210e74987397b4b9dcc
derhieakpos <derhieakpos@gmail.com>
2023-04-18 07:55:27 -0700

    Object-like Macro

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints its name.
+ * @argc: Argument counter.
+ * @argv: Argument variables.
+ *
+ * Return: Zero, EXIT_SUCCESS.
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,7 @@
+#ifndef MACRO_H
+#define MACRO_H
+
+#include "0-object_like_macro.h"
+#define SIZE 1024
+
+#endif /*MACRO_H*/
