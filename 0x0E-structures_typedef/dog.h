c5b3ebb93f4bf56bf52b4cc7cc1550c440fb47e7
derhieakpos <derhieakpos@gmail.com>
2023-04-19 22:54:40 -0700

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,13 +14,7 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-}
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
+};
+typedef struct dog my_dog;
 
 #endif
