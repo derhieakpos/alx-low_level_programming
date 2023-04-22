c928b32244082c31afadd18ae798e3f7ed459f75
derhieakpos <derhieakpos@gmail.com>
2023-04-21 19:20:04 -0700

    Print all

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,48 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_all - prints anything.
+ * @format: all the args type.
+ */
+void print_all(const char * const format, ...)
+{
+	int i = 0;
+	char *str;
+
+	va_list args;
+	va_start(args, format);
+
+	while (format && format[i] != '\0')
+	{
+		switch (format[i])
+		{
+			case 'f':
+				printf("%f", va_arg(args, double));
+				break;
+			case 'i':
+				printf("%d", va_arg(args, int));
+				break;
+			case 'c':
+				printf("%c", va_arg(args, int));
+				break;
+			case 's':
+				str = va_arg(args, char *);
+				if (str)
+				{
+					printf("%s", str);
+					break;
+				}
+				printf("(nil)");
+				break;
+			default:
+				i++;
+				continue;
+		}
+		if (format[i + 1] != '\0')
+			printf(", ");
+		i++;
+	}
+	printf("\n");
+	va_end(args);
+}
