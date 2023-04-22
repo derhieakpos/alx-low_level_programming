18646604d9a3d5d22631b0849ab33a0ea22fbd1c
derhieakpos <derhieakpos@gmail.com>
2023-04-21 19:51:17 -0700

    What's my name --Fixed

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -6,7 +6,7 @@
  * @f: A pointer function
  * Return: No return cause we've declared VOID as R_TYPE
  */
-void print_name(char *name, void (*f) (char *))
+void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
 
