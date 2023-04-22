6c2b66afe7492d05f5c8b9bfe74c14b42caba270
derhieakpos <derhieakpos@gmail.com>
2023-04-21 21:06:50 -0700

    Calc function

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -5,7 +5,7 @@
  * the operation asked by the user
  * @s: the operator given by the user
  *
- * Return: pointer to the function that corresponds to the 
+ * Return: pointer to the function that corresponds to the
  * operator given as a parameter
  */
 int (*get_op_func(char *s))(int, int)
