78bc59484af1ed1fe0752d0e11eef738178e4ddb
derhieakpos <derhieakpos@gmail.com>
2023-04-30 21:06:37 -0700

    The Hare and the Tortoise0

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void start(void)__attribute__((constructor));
+
+/**
+ * start - executes this function before the main is executed.
+ */
+void start(void)
+{
+	printf("You're beat! abd yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
