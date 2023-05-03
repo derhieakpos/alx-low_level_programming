f928f3ee440e59a6b1aa40adc1579c653aec22d4
derhieakpos <derhieakpos@gmail.com>
2023-05-02 21:56:58 -0700

    Unfinished loop find

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * listint_t *find_listint_loop - Finds the loop in a linked list.
+ * @head: the pointer
+ *
+ * Return: the address of the node.
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+
+}
