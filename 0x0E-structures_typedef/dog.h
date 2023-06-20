a7010e23e95cfa809a4b5e46b0506a7c3e328118
derhieakpos <derhieapos@gmail.com>
2023-06-19 22:00:24 -0700

    Struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - The basic info of a dog
+ * @name: First name
+ * @age: Age
+ * @owner: Ownwer fo the dog
+ *
+ * Description: Long Description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+typedef struct dog dog_t;
+
+#endif
