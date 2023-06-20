a50f134d1747397888ea95710f997582e1341366
derhieakpos <derhieapos@gmail.com>
2023-06-19 19:39:14 -0700

    File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,18 @@
+#ifndef MACRO_H
+#define MACRO_H
+
+#include <stdio.h>
+
+/**
+  * main - prints file name.
+  *
+  * Return: SUCCESS exit status.
+  */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
+
+#endif /*MACROI_H */
