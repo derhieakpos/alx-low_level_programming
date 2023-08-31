30dabfe1e0355cb52481bb27a2d0668e86b95d12
derhieakpos <derhieakpos@gmail.com>
2023-08-31 10:57:41 -0700

    Print binary and get binary

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,5 +2,8 @@
 #define MAIN_H
 
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int _putchar(char c);
+int get_bit(unsigned long int n, unsigned int index);
 
 #endif
